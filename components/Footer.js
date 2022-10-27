672f95eb515ffa2db393adf573a34406dfafb5db
A-Sync <kitkyatto@gmail.com>
2022-10-27 01:42:05 +0100

    meowmeowmeow5

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="w-screen flex place-content-center">
-        <div className=" transition w-36 ease-in-out duration-300 flex fixed place-content-center p-2 items-center font-mono text-[#ffdbb7] backdrop-brightness-110 scale-75 backdrop-blur-lg font-normal bottom-5 rounded-lg hover:scale-90">
+        <div className=" transition ease-in-out duration-300 flex fixed place-content-center p-2 items-center font-mono text-[#ffdbb7] backdrop-brightness-110 scale-75 backdrop-blur-lg font-normal bottom-5 rounded-lg hover:scale-90">
             ©️ Kyaliko 2022
         </div>
     </div>
