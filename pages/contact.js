2aaed44d55547a7a53362e403d1fd03f6992e317
A-Sync <106713009+Hi-doki@users.noreply.github.com>
2022-12-09 22:04:12 +0000

    meowmeowmeow29

--- pages/contact.js
+++ pages/contact.js
@@ -14,10 +14,23 @@ const contact = () => {
             </div>
             <div className="flex place-content-center gap-4 text-center pb-48">
                 <div className="bg-gray-50/10 backdrop-blur-lg p-2 rounded-md flex gap-4 items-center font-semibold hover:scale-110 transition ease-in-out duration-300">
-                    <img src="https://avatars.githubusercontent.com/u/106713009?v=4" className="w-12 rounded-full" />
-                    <div>
-                        <span>kyatto<span className="text-[#ffdbb7]">#2671</span></span>
+                    <div className="flex divide-x place-content-between justify-center items-center">
+                        <div className="flex pr-4">
+                            <img src="https://avatars.githubusercontent.com/u/106713009?v=4" className="w-24 rounded-full" />
+                        </div>
+                        <div className="grid pl-4 gap-y-2">
+                            <div className="flex gap-x-2">
+                                <img className="w-6" src="./discord-mark-white.svg" /><span>kyatto<span className="text-[#ffdbb7]">#2671</span></span>
+                            </div>
+                            <div className="flex gap-x-2">
+                                <img className="w-6" src="./Logo white.svg" /><span>calico_cl</span>
+                            </div>
+                            <div className="flex gap-x-2">
+                                <img className="w-6" src="./github-mark-white.svg" /><span>hi-doki</span>
+                            </div>
+                        </div>
                     </div>
+                    
                 </div>
             </div>
             <div className="flex place-content-center text-center pb-10">
