fd4765400dca56419e6c2bb27d7e1fbdf853b9c8
A-Sync <106713009+Hi-doki@users.noreply.github.com>
2022-12-17 17:57:24 +0000

    meowmeowmeow33

--- pages/meow.js
+++ pages/meow.js
@@ -18,7 +18,7 @@ const contact = () => {
 
 
 
-            <div className="flex place-content-center gap-4 text-center p-10">
+            <div className="flex place-content-center gap-4 text-center pb-10">
                 <div className="bg-gray-50/10 backdrop-blur-lg p-2 w-[25rem] md:w-[30rem] rounded-md grid md:flex gap-4 items-center font-semibold hover:scale-110 transition ease-in-out duration-300">
                     <div className="grid md:flex place-content-center gap-4">
                         <div className="flex pt-2 md:pt-0 place-content-center">
@@ -36,17 +36,43 @@ const contact = () => {
                                 
                             </div>
                             <div className="flex place-content-center md:place-content-start">
-                                <span className="text-grey-400 w-5/6  font-light">An open-source installer sdfsdf dfsdf sdffor programs.</span>
+                                <span className="text-grey-400 w-auto font-light">An open-source installer sdfsdf dfsdf sdffor programs.</span>
                             </div>
                             
                         </div>
                     </div>
                 </div>
-
-
-
-                
             </div>
+
+
+
+            <div className="flex place-content-center gap-4 text-center pb-10">
+                <div className="bg-gray-50/10 backdrop-blur-lg p-2 w-[25rem] md:w-[30rem] rounded-md grid md:flex gap-4 items-center font-semibold hover:scale-110 transition ease-in-out duration-300">
+                    <div className="grid md:flex place-content-center gap-4">
+                        <div className="flex pt-2 md:pt-0 place-content-center">
+                            <img className="w-24 rounded-md hover:scale-105 transition ease-in-out duration-300" src="https://avatars.githubusercontent.com/u/101818213?s=400&u=1be6149633a307b15871cb3e626de0a819054b04&v=4"></img>
+                        </div>
+                        
+                        <div className="grid gap-4 text-center md:text-left">
+                            <div className="grid md:flex gap-4 place-content-center md:place-content-between">
+                                <span className="font-semibold text-lg">Kyaliko RPC</span>
+                                <div className="md:absolute md:right-2">
+                                    <Link href="https://github.com/kyaliko/kyaliko-rpc">
+                                        <button className="p-1 bg-white bg-opacity-10 rounded-md w-24">Github</button>
+                                    </Link>
+                                </div>
+                                
+                            </div>
+                            <div className="flex place-content-center md:place-content-start">
+                                <span className="text-grey-400 w-auto font-light">A Discord RPC changer</span>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
 
             <div className="flex place-content-center gap-4 text-center pb-48">
                 <div className="bg-gray-50/10 backdrop-blur-lg p-2 w-[25rem] md:w-[30rem] rounded-md grid md:flex gap-4 items-center font-semibold hover:scale-110 transition ease-in-out duration-300">
@@ -70,7 +96,7 @@ const contact = () => {
                                 
                             </div>
                             <div className="flex place-content-center md:place-content-start">
-                                <span className="text-grey-400 w-5/6  font-light">A C# Minecraft Launcher.</span>
+                                <span className="text-grey-400 w-auto  font-light">A C# Minecraft Launcher.</span>
                             </div>
                             
                         </div>
