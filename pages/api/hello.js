d9b7d93122313b2b4f28b9d186daefc9b3f7534b
A-Sync <106713009+Hi-doki@users.noreply.github.com>
2023-01-04 18:07:38 +0000

    meowmeowmeow35

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ discord: 'GX6TVXafUm', webversion: "0.35" })
+  res.status(200).json({ discord: 'GX6TVXafUm', webversion: "0.34" })
 }
